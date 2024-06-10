7fd7bd4aab4e3dc371004adc90503e6b00e14444
dannnik4 <daniilperstnev2001@gmail.com>
2024-06-10 20:16:01 +0300

    Project updated

--- src/main/java/com/example/colorsattentiontest/ColorController.java
+++ src/main/java/com/example/colorsattentiontest/ColorController.java
@@ -1,10 +1,11 @@
 package com.example.colorsattentiontest;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
+import javafx.animation.*;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -16,8 +17,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
 
 public class ColorController {
 
